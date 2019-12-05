a1c5f0eb9020ac074d7d6e726360a238cca92669
qwertyakash <akash.wagh1303@gmail.com>
2019-12-05 06:27:47 -0800

    calculator file

---
+++ cal.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+int a,b,add,sub,div,mul;
+	printf("Enter two numbers : ");
+	scanf("%d %d",&a,&b);
+		add=a+b;
+		sub=a-b;
+		div=a/b;
+		mul=a*b;
+	printf("addition = %d",add);
+	printf("\nsubtraction = %d",sub);
+	printf("\ndivision = %d",div);
+	printf("\nmultiplication = %d",mul);
+return 0;
+}
